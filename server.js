31b41884a3a91808cb90bd090a415aea26617818
정회석 <jsk2343@naver.com>
2024-03-28 00:45:43 +0900

    [개발] - 웹소켓 서버 최적화

--- server.js
+++ server.js
@@ -1,33 +1,55 @@
 import express from 'express';
-import { createServer } from 'http';
-import socketIO from 'socket.io';
+import http from 'http';
+import { Server } from 'socket.io';
 import cors from 'cors';
+import UAParser from 'ua-parser-js';
+import { randomBytes } from 'crypto';
+import moment from 'moment';
+import 'moment-timezone';
+
+const parser = new UAParser();
 
 const app = express();
 app.use(cors());
 
-const server = createServer(app);
-const io = socketIO(server, {
+const server = http.createServer(app);
+const io = new Server(server, {
     cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
     },
 });
 
-io.on('connection', (socket) => {
-    console.log('New client connected');
+//접속 정보 hash map
+const accessor = new Map();
 
-    socket.on('message', (message) => {
-        console.log('Received message:', message);
-        socket.broadcast.emit('message', message);
-    });
+io.on('connection', (socket) => {
+    const time = moment().tz('Asia/Seoul').format();
+    console.log('--웹 소켓 연결 성공--');
+    //socket info
+    const id = randomBytes(8).toString('hex');
+    const userAgent = socket.handshake.headers['user-agent'];
+    //userAgent set
+    parser.setUA(userAgent);
+    const resultAgent = parser.getResult();
+
+    const json = {
+        browser: resultAgent.browser.name,
+        os: resultAgent.os.name,
+        connectionTime: time,
+    }
+
+    accessor.set(id, json);
+
+    io.emit('accessor', Array.from(accessor, ([id, json]) => ({ id, ...json })));
 
     socket.on('disconnect', () => {
-        console.log('Client disconnected');
+        accessor.delete(id);
+        io.emit('accessor', Array.from(accessor, ([id, json]) => ({ id, ...json })));
     });
 });
 
 const port = 8080;
 server.listen(port, () => {
-    console.log(`Socket.IO server running on port ${port}`);
+    console.log(`Socket.IO 서버 시작 포트 번호 - ${port}`);
 });
